67493b76c14540350508952e9d78f4ebb7414474
Shubham Kumar <shubham.kr2951@gmail.com>
2023-06-10 12:41:10 +0530

    added quote

---
+++ src/components/Quote.jsx
@@ -0,0 +1,13 @@
+import styles from "./Quote.module.css";
+
+const Quote = () => (
+  <section className={styles.quote}>
+    <p>
+      With a menu featuring an array of fresh, locally sourced ingredients, we
+      aim to provide you with an exceptional dining experience that will leave
+      you coming back for more. Come and savor the flavors of our food and the
+      warmth of our hospitality at our restaurant.
+    </p>
+  </section>
+);
+export default Quote;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
 import Hero from "../components/Hero";
+import Quote from "../components/Quote";
 
 const HomePage = () => (
   <>
     <Hero />
+    <Quote />
   </>
 );
 export default HomePage;
